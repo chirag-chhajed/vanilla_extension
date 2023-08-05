87736b8f4356c97708a7cb9b3e94a01c7062df8f
Chirag Chhajed <chhajedchirag18@gmail.com>
2023-08-05 10:35:12 +0530

    added some bundling options

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import manifest from "./manifest.json";
 
 export default defineConfig({
   plugins: [crx({ manifest })],
+  build: {
+    rollupOptions: {
+      // Add index.html as an additional resource to be bundled
+      input: {
+        main: "./main.js",
+        index: "./index.html",
+      },
+    },
+  },
 });
